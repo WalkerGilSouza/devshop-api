43888ae3f321723ffcacbc68c9afbd0beb19dcb4
Walker Gil <53378719+WalkerGilSouza@users.noreply.github.com>
2021-04-07 20:23:26 -0300

    atualizações

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { GraphQLModule } from '@nestjs/graphql'
+import { CategoryModule } from './category/category.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true
+      })
+    }),
+    /*
+    TypeOrmModule.forRoot({
+      x
+      type: 'postgres',
+      url: 'postgres://postgres:"senha"@localhost:5432/devshop',
+      autoLoadEntities: true,
+      synchronize: true,
+      //entities: [Category],
+      logging: true
+    }),*/
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql'
+    }),
+    CategoryModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
